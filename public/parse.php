ed212d8c9697a5aa676e42bf2448421cfc79c3ef
Mark Harwood <mark@phunky.co.uk>
2014-05-05 23:37:20 +0100

    Only save to csv when im online or playing, also ignore the data dir

--- public/parse.php
+++ public/parse.php
@@ -11,26 +11,30 @@ $response = $request->send();
 
 $crawler = new Crawler($response->getBody(true));
 
-$results = [
-  'timestamp' => date("Y-m-d H:i:s"),
-  'online' => 1,
-  'playing' => 0
-];
+$results = null;
 
 $crawler->filter('.profile_in_game.persona.online')->filter('.profile_in_game.persona.online')->each(function($node, $i) use (&$results) {
-  $results['online'] = 1;
-  $results['playing'] = 0;
+  $results = [
+    'timestamp' => date("Y-m-d H:i:s"),
+    'online' => 1,
+    'playing' => 0
+  ];
 });
 
 $crawler->filter('.profile_in_game.persona.in-game .profile_in_game_name')->each(function($node, $i) use (&$results) {
-  $results['online'] = 1;
-  $results['playing'] = trim( $node->text() );
+  $results = [
+    'timestamp' => date("Y-m-d H:i:s"),
+    'online' => 1,
+    'playing' => trim( $node->text() )
+  ];
 });
 
-// Write CSV
-$fp = fopen('../data/playing.csv', 'a');
-fputcsv($fp, $results);
-fclose($fp);
+if($results){
+  // Write CSV
+  $fp = fopen('../data/playing.csv', 'a');
+  fputcsv($fp, $results);
+  fclose($fp);
+}
 
 // Notes
 // profile_in_game persona in-game | online | offline

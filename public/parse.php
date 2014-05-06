c855b6764d28067c583670a1936c9a270c13cb98
Mark Harwood <mark@phunky.co.uk>
2014-05-06 08:57:36 +0100

    Add some comments for whats going on

--- public/parse.php
+++ public/parse.php
@@ -1,17 +1,20 @@
 <?php
-// Bring in anything from Composer
+
+// Bring in anything from Composer and derps
 require_once '../vendor/autoload.php';
 
 use Guzzle\Http\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
+// Go grab my steam profile
 $client = new Client('http://steamcommunity.com/id/phunky/');
 $request = $client->get();
 $response = $request->send();
 
+// Grab the response body
 $crawler = new Crawler($response->getBody(true));
 
-// If my profile says i'm in game, store which game i'm playing
+// If my profile says i'm in game, store which game i'm currently playing
 $crawler->filter('.profile_in_game.persona.in-game .profile_in_game_name')->each(function($node, $i){
   $fp = fopen('../data/playing.csv', 'a');
   fputcsv($fp, [

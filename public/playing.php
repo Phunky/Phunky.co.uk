0c3250f3a771a186cf5466fe91be1f11d6dbe6e4
Mark Harwood <mark@phunky.co.uk>
2014-05-09 23:58:10 +0100

    Argh

--- public/playing.php
+++ public/playing.php
@@ -15,10 +15,14 @@ echo '<ul>';
 foreach($logs as $log){
   echo '<li>';
     echo '<h2>' . $log->game . '</h2>';
-    $started = Carbon::createFromFormat('Y-m-d H:i:s', $log->started);
-    $stopped = Carbon::createFromFormat('Y-m-d H:i:s', $log->stopped);
-    $played = $started->diffInMinutes($stopped);
-    echo 'played for ' . $played  . ' minutes, started playing at ' . $log->started . ' and stopped at ' . $log->stopped;
+    if($log->stopped){
+      $started = Carbon::createFromFormat('Y-m-d H:i:s', $log->started);
+      $stopped = Carbon::createFromFormat('Y-m-d H:i:s', $log->stopped);
+      $played = $started->diffInMinutes($stopped);
+      echo 'played for ' . $played  . ' minutes, started playing at ' . $log->started . ' and stopped at ' . $log->stopped;
+    } else {
+      echo 'Currently playing now!'
+    }
   echo '</li>';
 }
 echo '</ul>';

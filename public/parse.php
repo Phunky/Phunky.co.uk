4ece2cc0ec10401842990226a59c47424ded9e86
Mark Harwood <mark@phunky.co.uk>
2014-07-23 16:50:54 +0100

    Change up parsing script to support multiple users and expand out games from inital logging

--- public/parse.php
+++ public/parse.php
@@ -10,61 +10,78 @@ use Carbon\Carbon;
 // Beans, beans, beans... fart
 R::setup('mysql:host=localhost;dbname=playing','phunky','sl1200m2');
 
-$log = R::dispense('log');
+$users = R::findAll('users');
 
-// Go grab my steam profile
-$client = new Client('http://steamcommunity.com/id/phunky/');
-$request = $client->get();
-$response = $request->send();
+// Loop through and grab that shit.
+foreach($users as $user){
 
-// Grab the response body
-$crawler = new Crawler($response->getBody(true));
+  // Go grab my steam profile
+  $client = new Client('http://steamcommunity.com/id/' . $user->community_id . '/');
+  $request = $client->get();
+  $response = $request->send();
 
-// If my profile says i'm in game, store which game i'm currently playing
-$crawler->filter('.profile_in_game.persona.in-game .profile_in_game_name')->each(function($node, $i) use ($crawler) {
-  $playing = trim( $node->text() );
-  $log = R::findLast('log');
+  // Grab the response body
+  $crawler = new Crawler($response->getBody(true));
 
-  // No last played game (first parse)
-  // or we're playing a different game from the last one
-  if( !$log || $log->game !== $playing || $log->stopped !== null ){
+  // Grab last game from db
+  $last_log = R::findLast('log', ' user_id = ? ', [ $user->id ]);
 
-    // We're not playing the same game as before
-    // So stop it
-    if( $log && !$log->stopped ){
-      $log->stopped = date("Y-m-d H:i:s");
+  // If my profile says i'm in game, store which game i'm currently playing
+  $crawler->filter('.profile_in_game.persona.in-game .profile_in_game_name')->each(function($node, $i) use ($user, $crawler, $last_log) {
+    $game_name = trim( $node->text() );
+
+    $playing = R::findLast('log', ' name = ? ', [ $game_name ]);
+
+    // No last played game (first parse)
+    // or we're playing a different game from the last one
+    if( !$last_log || $last_log->game !== $playing || $last_log->stopped !== null ){
+
+      // We're not playing the same game as before
+      // So stop it
+      if( $last_log && !$last_log->stopped ){
+        $last_log->stopped = date("Y-m-d H:i:s");
+        R::store( $last_log );
+      }
+
+      // Couldn't find the game, so store it as a new one
+      if( !$playing ){
+        $playing = R::dispense('games');
+        $playing->name = $game_name;
+        R::store( $playing );
+      }
+
+      // New log for new game
+      $log = R::dispense('log');
+      $log->started = date("Y-m-d H:i:s");
+      $log->game = $playing;
+      $log->user = $user;
       R::store( $log );
-    }
 
-    // New log for new game
-    $log = R::dispense('log');
-    $log->started = date("Y-m-d H:i:s");
-    $log->game = $playing;
-
-    $recent = $crawler->filter('.recent_games .game_name a')->eq(0);
-    if( $recent && trim( $recent->text() ) === $playing ){
-      $href = $recent->attr('href');
-      if( $href ){
-        $explode = explode('/', $recent->attr('href'));
-        $log->appid = end( $explode );
+      $recent = $crawler->filter('.recent_games .game_name a')->eq(0);
+      if( $recent && trim( $recent->text() ) === $playing ){
+        $href = $recent->attr('href');
+        if( $href ){
+          $explode = explode('/', $recent->attr('href'));
+          $playing->app_id = end( $explode );
+          R::store( $playing );
+        }
       }
     }
-  }
 
-  // Last seen for non-stopped games
-  if( !$log->stopped ){
-    $log->last_seen = date("Y-m-d H:i:s");
-    R::store( $log );
-  }
-});
+    // Last seen for non-stopped games
+    if( !$log->stopped ){
+      $log->last_seen = date("Y-m-d H:i:s");
+      R::store( $log );
+    }
+  });
 
-$crawler->filter('.profile_in_game.persona.offline, .profile_in_game.persona.online')->each(function($node, $i){
-  $log = R::findLast('log');
+  $crawler->filter('.profile_in_game.persona.offline, .profile_in_game.persona.online')->each(function($node, $i) use ($last_log) {
 
-  if( $log && $log->stopped === null ){
-    $log->stopped = date("Y-m-d H:i:s");
-    R::store( $log );
-  }
+    if( $last_log && $last_log->stopped === null ){
+      $last_log->stopped = date("Y-m-d H:i:s");
+      R::store( $last_log );
+    }
 
-});
+  });
 
+}
